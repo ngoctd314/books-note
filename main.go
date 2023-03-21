050d0c09012bf914616644fd26c7e752f155b2d5
tranngoc <tranngoc.contact@gmail.com>
2023-03-21 20:39:49 +0700

    add kafka

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type T struct {
+	X    int  `max:"99" min:"0" default:"0"`
+	Y, Z bool `optional:"yes"`
+}
 
 func main() {
-	var data int
-	go func() {
-		data++
-	}()
-	if data == 0 {
-		fmt.Printf("the value is %v\n", data)
-	}
+	t := reflect.TypeOf(T{})
+	x := t.Field(0).Tag
+	y := t.Field(1).Tag
+	z := t.Field(2).Tag
+
+	fmt.Println(x, y, z)
+	fmt.Println(reflect.TypeOf(x))
+	v, present := x.Lookup("max")
+	fmt.Println(len(v), present, v)
+	fmt.Println(x.Get("max"))
+	fmt.Println(x.Lookup("optional"))
+	fmt.Println(y.Lookup("optional"))
+	fmt.Println(z.Lookup("optional"))
 }
